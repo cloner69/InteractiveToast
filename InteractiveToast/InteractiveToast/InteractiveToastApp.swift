b4e7e4a8dca57761173ec0a1695cdb328a94e5ff
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-03 13:34:30 +0700

    interactive toast

---
+++ InteractiveToast/InteractiveToast/InteractiveToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveToastApp.swift
+//  InteractiveToast
+//
+//  Created by Adrian Suryo Abiyoga on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
